bfec5e1baccdf7d4693a59dc908818c529b29d3f
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-3.local>
2021-06-07 14:54:15 +0100

    setup fetch request and save data to hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState({});
+
+  const handleFetch = async () => {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const data = await response.json();
+    setData(data);
+  };
+
   return (
     <div>
       <h1>Chuck Norris Quote Machine</h1>
+      <button onClick={handleFetch}>handleFetch</button>
+      {data.value && (
+        <>
+          <h2>Joke:</h2>
+          <p>{data.value}</p>
+        </>
+      )}
     </div>
   );
 };
